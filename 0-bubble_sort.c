37789cb65761d1c59f9f4d8c96210a06b2dfa900
aliqulamaliyev <aliqulamaliyev@gmail.com>
2025-08-17 09:32:49 +0000

    completes all c tasks

---
+++ 0-bubble_sort.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * bubble_sort - Sorts an array using Bubble sort algorithm
+ * @array: The array to be sorted
+ * @size: Size of the array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+		return;
+
+	size_t i, j;
+	int temp;
+	int swapped;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		swapped = 0;
+		for (j = 0; j < size - 1 - i; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				swapped = 1;
+				print_array(array, size);
+			}
+		}
+		if (!swapped)
+			break;
+	}
+}
